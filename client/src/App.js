1d8933b726346cb4e8b37f5efac12039df240d09
shsxol <shrestha.smile31@gmail.com>
2023-08-02 14:08:20 +0545

    React list

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import React from "react";
 import Title from "./component/Title";
 import Example from "./component/Example";
 
-const cars =['Ford', 'BMW', 'Audi']
+// const cars =['Ford', 'BMW', 'Audi', 'Tesla', 'Ferari', ]
 
 
 function App() {
   return (
     <>
       <Title name="TODO App" />
-      <Example cars= {cars}/>
+      <Example isgoal= {true}/>
     </>
   );
 }
--- client/src/component/Example.js
+++ client/src/component/Example.js
@@ -28,20 +28,57 @@
 // }
 
 
-// && logical operator
-const cars =['Ford', 'BMW', 'Audi'];
+// // && logical operator
+// const cars =['Ford', 'BMW', 'Audi'];
 
-<Garage cars ={cars}/>
-    export default function Garage(props){
-    const cars = props.cars;
+// <Garage cars ={cars}/>
+//     export default function Garage(props){
+//     const cars = props.cars;
+//     return (
+//         <>
+//         <h1> Garage </h1>
+//         {cars.length > 0 && 
+//         <h2>
+//             You have {cars.length} cars in your garage. 
+//         </h2>
+//         }
+//     </>
+//     );
+// }
+
+// // ternary operator 
+
+// export default function Goal(props) {
+//     const isgoal= props.isgoal;
+//     return(
+//         <>
+//         {isgoal ? <MadeGoal/> : <MissedGoal/> }
+//         </>
+//     );
+// }
+
+// function MissedGoal(){
+//      return <h1> MISSED! </h1>;
+// }
+
+// function MadeGoal(){
+//      return <h1> Goal! </h1> 
+// }
+
+// React list
+
+export default function Garage(){
+    const cars =['Ford','BMW', 'Audi', 'Tesla'];
     return (
         <>
-        <h1> Garage </h1>
-        {cars.length > 0 && 
-        <h2>
-            You have {cars.length} cars in your garage. 
-        </h2>
-        }
-    </>
+        <h1> Who lives in my garage? </h1>
+        <ul>
+            {cars.map((car) => <Car brand ={car} /> )}
+        </ul>
+        </>
     );
+}
+
+function Car(props){
+    return <li>I am  {props.brand} </li>;
 }
